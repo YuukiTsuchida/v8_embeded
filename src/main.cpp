4fefd5964c55c5c1f8e03461a2fc1ba5e65da337
YuukiTsuchida <dev.yuukitsuchida@gmail.com>
2020-02-09 05:30:30 +0900

    load javascript code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
 #include <v8.h>
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <string_view>
 #include "engine/engine.hpp"
+
+std::string load_file(std::string_view file_name) {
+  std::stringstream file_string;
+  std::ifstream stream;
+  stream.open(file_name.data(), std::ios::in);
+  std::string line("");
+  while (std::getline(stream, line)) {
+    file_string << line << "\n";
+  }
+
+  return file_string.str();
+}
 
 int main(int argc, char* argv[]) {
   std::cout << argv[0] << std::endl;
   engine::engine engine_instance;
   engine_instance.initialize(argv[0]);
-
-  engine_instance.run_js_code("1 + 1");
+  std::string code = load_file("resource/main.js");
+  engine_instance.run_js_code(code);
   engine_instance.dispose();
 
 #if WIN
