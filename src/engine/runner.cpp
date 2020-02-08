c5efa067be26eee281931da2454f892472b873ef
YuukiTsuchida <dev.yuukitsuchida@gmail.com>
2020-02-09 04:24:14 +0900

    binding object to context

--- src/engine/runner.cpp
+++ src/engine/runner.cpp
@@ -1,38 +1,4 @@
-﻿#include "runner.hpp"
-#include <iostream>
-#include "v8_utils.hpp"
-
+﻿
 namespace engine {
-
-runner::runner(v8::Isolate* isolate)
-    : context_(isolate, v8::Context::New(isolate, nullptr)) {}
-
-void runner::run(v8::Isolate* isolate, std::string_view code) {
-  auto context = context_.Get(isolate);
-  v8::Context::Scope scope(context);
-
-  auto source = utils::to_v8_string(isolate, code);
-
-  // Compile the source code.
-  v8::TryCatch try_catch(isolate);
-  auto script = v8::Script::Compile(context, source);
-
-  if (script.IsEmpty()) {
-    utils::exception_log(isolate, try_catch);
-    return;
-  }
-
-  // Run the script to get the result.
-  auto result = script.ToLocalChecked()->Run(context).ToLocalChecked();
-  if (result.IsEmpty()) {
-    utils::exception_log(isolate, try_catch);
-    return;
-  }
-
-  if (result->IsNumber()) {
-    auto value = v8::Local<v8::Uint32>::Cast(result)->Value();
-    std::cout << value << std::endl;
-  }
-}
 
 }  // namespace engine
--- src/engine/runner.hpp
+++ src/engine/runner.hpp
@@ -2,13 +2,63 @@
 
 #include <v8.h>
 #include <string_view>
+#include "v8_utils.hpp"
 
 namespace engine {
 
 class runner {
  public:
-  explicit runner(v8::Isolate* isolate);
-  void run(v8::Isolate* isolate, std::string_view code);
+  explicit runner(v8::Isolate* isolate)
+      : context_(isolate, v8::Context::New(isolate, nullptr)) {}
+
+  template <class Arg>
+  void bind(v8::Isolate* isolate, v8::Local<v8::Context> context, Arg arg) {
+    arg->bind(isolate, context);
+  }
+
+  template <class Head, class... Tail>
+  void bind(v8::Isolate* isolate, v8::Local<v8::Context> context, Head head,
+            Tail... tail) {
+    head->bind(isolate, context);
+    bind(isolate, context, tail);
+  }
+
+  void run_code(v8::Isolate* isolate, v8::Local<v8::Context> context,
+                std::string_view code) {
+    auto source = utils::to_v8_string(isolate, code);
+
+    // Compile the source code.
+    v8::TryCatch try_catch(isolate);
+    auto script = v8::Script::Compile(context, source);
+
+    if (script.IsEmpty()) {
+      utils::exception_log(isolate, try_catch);
+      return;
+    }
+
+    // Run the script to get the result.
+    auto result = script.ToLocalChecked()->Run(context).ToLocalChecked();
+    if (result.IsEmpty()) {
+      utils::exception_log(isolate, try_catch);
+      return;
+    }
+  }
+
+  void execute(v8::Isolate* isolate, std::string_view code) {
+    auto context = context_.Get(isolate);
+    v8::Context::Scope scope(context);
+
+    run_code(isolate, context, code);
+  }
+
+  template <class... Args>
+  void execute(v8::Isolate* isolate, std::string_view code, Args... args) {
+    auto context = context_.Get(isolate);
+    v8::Context::Scope scope(context);
+
+    bind(isolate, context, args...);
+    run_code(isolate, context, code);
+  }
 
  private:
   v8::Global<v8::Context> context_;
