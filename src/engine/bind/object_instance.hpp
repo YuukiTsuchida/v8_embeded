802076df9c58036abd9755a64f696d23d57c55a7
YuukiTsuchida <dev.yuukitsuchida@gmail.com>
2020-02-19 08:46:29 +0900

    add local object binding system

---
+++ src/engine/bind/object_instance.hpp
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <v8.h>
+#include <type_traits>
+#include "../v8_utils.hpp"
+#include "global_object_instance.hpp"
+
+namespace engine::bind {
+
+template <class Type>
+class object_instance {
+ public:
+  template <class... Args>
+  explicit object_instance(v8::Isolate* isolate, v8::Local<v8::Object> object,
+                           const Args&... args)
+      : object_(object), instance_(args...) {
+    object->SetInternalField(0, v8::External::New(isolate, &instance_));
+  }
+
+  template <class Context,
+            std::enable_if_t<std::is_same_v<Context, v8::Local<v8::Context>>,
+                             std::nullptr_t> = nullptr>
+  void bind(v8::Isolate* isolate, Context context) {
+    instance_.bind_instance(isolate, *this);
+    context->Global()->Set(utils::to_v8_string(isolate, Type::object_name),
+                           object_);
+  }
+
+  template <class ValueType>
+  void bind(v8::Isolate* isolate, global_object_instance<ValueType>& parent) {
+    instance_.bind_instance(isolate, *this);
+    parent.object_->Set(utils::to_v8_string(isolate, Type::object_name),
+                        object_);
+  }
+
+ private:
+  v8::Local<v8::Object> object_;
+  Type instance_;
+};
+
+}  // namespace engine::bind
---
+++ src/engine/bind/object_template.hpp
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <v8.h>
+#include <cassert>
+#include "object_template_binder.hpp"
+
+namespace engine::bind {
+
+template <class Type>
+class object_template {
+ public:
+  explicit object_template() = default;
+  explicit object_template(v8::Isolate* isolate) { initialize(isolate); }
+
+  void initialize(v8::Isolate* isolate) {
+    if (!object_template_.IsEmpty()) {
+      assert(false);
+    }
+
+    object_template_ = v8::ObjectTemplate::New(isolate);
+    object_template_->SetInternalFieldCount(1);
+
+    object_template_binder<Type> binder(object_template_);
+    binder.bind(isolate);
+  }
+
+  v8::Local<v8::Object> create(v8::Isolate* isolate) const {
+    return object_template_->NewInstance(isolate->GetCurrentContext()).ToLocalChecked();
+  }
+
+ private:
+  v8::Local<v8::ObjectTemplate> object_template_;
+};
+
+}  // namespace engine::bind
